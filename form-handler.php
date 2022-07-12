f0f48ef7bb950d07af12b505de4b2d6f217bbbfb
Jyotsnapnr <33445626+Jyotsnapnr@users.noreply.github.com>
2022-07-12 15:52:50 -0600

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com';
+$email_from = 'jorakesh11@gmail.com';
 
 $email_subject = 'New Form Submission';
 
